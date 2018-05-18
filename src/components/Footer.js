7f267ff65a5f3023e07c79357286952240e181c1
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-05-18 23:34:48 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,8 @@ class Footer extends Component {
     return (
       <footer className="footer">
         <p>
-          Įrankį sukūrė Jelizaveta Potapova. Atsiliepimai/pasiūlymai laukiami el. paštu potapova.jelizaveta@gmail.com
+          Paieškos įrankį sukūrė Jelizaveta Potapova. Kavinių sąrašas paimtas iš <a href="http://www.auginupametinukus.lt/titulinis/vaikams-draugisku-kaviniu-lietuvoje-sarasas/" target="_blank">"Auginu Pametinukus"</a>.
+          <br/>Atsiliepimai/pasiūlymai laukiami el. paštu potapova.jelizaveta@gmail.com
         </p>
         <p> </p>
       </footer>
